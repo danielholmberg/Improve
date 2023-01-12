e0d1af75e7745e50505083841a00f8da375c38a2
Daniel Holmberg <danielholmberg.dev@gmail.com>
2023-01-12 23:08:40 +0100

    Converts Util-class ContactInputValidator to Kotlin

--- app/src/main/java/dev/danielholmberg/improve/Utilities/ContactInputValidator.kt
+++ app/src/main/java/dev/danielholmberg/improve/Utilities/ContactInputValidator.kt
@@ -1,48 +1,40 @@
-package dev.danielholmberg.improve.Utilities;
+package dev.danielholmberg.improve.Utilities
 
-import android.content.Context;
-import androidx.appcompat.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Patterns;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
-import dev.danielholmberg.improve.Models.Company;
-import dev.danielholmberg.improve.R;
+import android.content.Context
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.Spinner
+import dev.danielholmberg.improve.R
+import dev.danielholmberg.improve.Models.Company
+import androidx.appcompat.app.AppCompatActivity
+import android.view.WindowManager
 
 /**
  * Class ${CLASS}
  */
+class ContactInputValidator(private val context: Context, inputLayoutContainer: View) {
+    private val inputLayoutContainer: View
+    private val inputName: EditText
+    private val inputEmail: EditText
+    private val inputPhone: EditText
+    private val spinnerCompany: Spinner
 
-public class ContactInputValidator {
-    private static final String TAG = ContactInputValidator.class.getSimpleName();
-
-    private View inputLayoutContainer;
-    private Context context;
-
-    private EditText inputName, inputEmail, inputPhone;
-    private Spinner spinnerCompany;
-    private Button addCompanyBtn;
-
-    public ContactInputValidator(Context context, View inputLayoutContainer) {
-        this.context = context;
-        this.inputLayoutContainer = (View) inputLayoutContainer;
-
-        inputName = (EditText) inputLayoutContainer.findViewById(R.id.input_name);
-        spinnerCompany = (Spinner) inputLayoutContainer.findViewById(R.id.spinner_company);
-        inputEmail = (EditText) inputLayoutContainer.findViewById(R.id.input_email);
-        inputPhone = (EditText) inputLayoutContainer.findViewById(R.id.input_mobile);
+    init {
+        this.inputLayoutContainer = inputLayoutContainer
+        inputName = inputLayoutContainer.findViewById<View>(R.id.input_name) as EditText
+        spinnerCompany = inputLayoutContainer.findViewById<View>(R.id.spinner_company) as Spinner
+        inputEmail = inputLayoutContainer.findViewById<View>(R.id.input_email) as EditText
+        inputPhone = inputLayoutContainer.findViewById<View>(R.id.input_mobile) as EditText
     }
 
     /**
      * Validating new contact form.
      * return true if both Name is not empty and each additional input is correct format.
      */
-    public boolean formIsValid() {
-        return validateName() && validateCompany() && validateEmail() && validatePhone();
+    fun formIsValid(): Boolean {
+        return validateName() && validateCompany() && validateEmail() && validatePhone()
     }
 
     /**
@@ -50,88 +42,85 @@ public class ContactInputValidator {
      * Validate if the user has entered a name.
      * @return false if name field is empty.
      */
-    private boolean validateName() {
-        String name = inputName.getText().toString().trim();
-
+    private fun validateName(): Boolean {
+        val name = inputName.text.toString().trim { it <= ' ' }
         if (TextUtils.isEmpty(name)) {
-            inputName.setError(context.getString(R.string.err_msg_name));
-            requestFocus(inputName);
-            return false;
+            inputName.error = context.getString(R.string.err_msg_name)
+            requestFocus(inputName)
+            return false
         }
-
-        return true;
+        return true
     }
 
-    private boolean validateCompany() {
-        Company selectedCompany = (Company) spinnerCompany.getSelectedItem();
-        if(selectedCompany == null) {
-            requestFocus(spinnerCompany);
-
-            if (spinnerCompany.getChildCount() > 0) {
-                spinnerCompany.performClick();
+    private fun validateCompany(): Boolean {
+        val selectedCompany = spinnerCompany.selectedItem as Company
+        if (selectedCompany == null) {
+            requestFocus(spinnerCompany)
+            if (spinnerCompany.childCount > 0) {
+                spinnerCompany.performClick()
             }
-            return false;
+            return false
         }
-        return true;
+        return true
     }
 
     /**
      * Validate if the user has entered a correct email-format.
      * @return true if company is valid.
      */
-    private boolean validateEmail() {
-        String email = inputEmail.getText().toString().trim();
-
-        if(TextUtils.isEmpty(email)) {
-          return true;
+    private fun validateEmail(): Boolean {
+        val email = inputEmail.text.toString().trim { it <= ' ' }
+        if (TextUtils.isEmpty(email)) {
+            return true
         } else if (!isValidEmail(email)) {
-            inputEmail.setError(context.getString(R.string.err_msg_email));
-            requestFocus(inputEmail);
-            return false;
+            inputEmail.error = context.getString(R.string.err_msg_email)
+            requestFocus(inputEmail)
+            return false
         }
-
-        return true;
-    }
-
-    /**
-     * @return true if the email is of correct format.
-     */
-    private static boolean isValidEmail(String email) {
-        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        return true
     }
 
     /**
      * Validate if the user has entered a non-empty phone number.
      * @return true if phone is not empty.
      */
-    private boolean validatePhone() {
-        String phone = inputPhone.getText().toString().trim();
-
-        if(TextUtils.isEmpty(phone)) {
-            return true;
-        } else if(!isValidPhone(phone)) {
-            inputPhone.setError(context.getString(R.string.err_msg_phone));
-            requestFocus(inputPhone);
-            return false;
+    private fun validatePhone(): Boolean {
+        val phone = inputPhone.text.toString().trim { it <= ' ' }
+        if (TextUtils.isEmpty(phone)) {
+            return true
+        } else if (!isValidPhone(phone)) {
+            inputPhone.error = context.getString(R.string.err_msg_phone)
+            requestFocus(inputPhone)
+            return false
         }
-
-        return true;
-    }
-
-    /**
-     * @return true if the phone is of correct format.
-     */
-    private static boolean isValidPhone(String phone) {
-        return !TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches();
+        return true
     }
 
     /**
      * Requests focus of the incoming view.
      * @param view
      */
-    private void requestFocus(View view) {
+    private fun requestFocus(view: View) {
         if (view.requestFocus()) {
-            ((AppCompatActivity) context).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+            (context as AppCompatActivity).window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        }
+    }
+
+    companion object {
+        private val TAG = ContactInputValidator::class.java.simpleName
+
+        /**
+         * @return true if the email is of correct format.
+         */
+        private fun isValidEmail(email: String): Boolean {
+            return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        }
+
+        /**
+         * @return true if the phone is of correct format.
+         */
+        private fun isValidPhone(phone: String): Boolean {
+            return !TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches()
         }
     }
 }
