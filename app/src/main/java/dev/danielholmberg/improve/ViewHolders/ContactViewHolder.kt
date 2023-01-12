47b4984bdd18a2fee33dc574bfb458b4d97ade35
Daniel Holmberg <danielholmberg.dev@gmail.com>
2023-01-12 14:56:58 +0100

    Converts ContactViewHolder to Kotlin

--- app/src/main/java/dev/danielholmberg/improve/ViewHolders/ContactViewHolder.kt
+++ app/src/main/java/dev/danielholmberg/improve/ViewHolders/ContactViewHolder.kt
@@ -1,112 +1,102 @@
-package dev.danielholmberg.improve.ViewHolders;
+package dev.danielholmberg.improve.ViewHolders
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import dev.danielholmberg.improve.Improve.Companion.instance
+import androidx.recyclerview.widget.RecyclerView
+import dev.danielholmberg.improve.Models.Contact
+import dev.danielholmberg.improve.R
+import android.widget.TextView
+import android.os.Bundle
+import dev.danielholmberg.improve.Fragments.ContactDetailsSheetFragment
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
-import androidx.recyclerview.widget.RecyclerView;
-
-import dev.danielholmberg.improve.Models.Contact;
-import dev.danielholmberg.improve.Fragments.ContactDetailsSheetFragment;
-import dev.danielholmberg.improve.Improve;
-import dev.danielholmberg.improve.R;
-
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
-
-public class ContactViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-
-    private View mView;
-
-    private Contact contact;
-
-    public ContactViewHolder(View itemView) {
-        super(itemView);
-        mView = itemView;
-    }
+class ContactViewHolder(private val mView: View) : RecyclerView.ViewHolder(
+    mView
+), View.OnClickListener {
+    private var contact: Contact? = null
 
     // OBS! Due to RecyclerView:
     // We need to define all views of each contact!
     // Otherwise each contact view won't be unique.
-    public void bindModelToView(final Contact contact) {
-        this.contact = contact;
+    fun bindModelToView(contact: Contact) {
+        this.contact = contact
 
         // [START] All views of a contact
-        Button callBtn = mView.findViewById(R.id.call_contact_btn);
-        Button mailBtn = mView.findViewById(R.id.mail_contact_btn);
-
-        TextView name = mView.findViewById(R.id.name_tv);
+        val callBtn = mView.findViewById<Button>(R.id.call_contact_btn)
+        val mailBtn = mView.findViewById<Button>(R.id.mail_contact_btn)
+        val name = mView.findViewById<TextView>(R.id.name_tv)
         // [END] All views of a contact
 
         // [START] Define each view
-        name.setText(contact.getName());
-
-        if (contact.getEmail() == null || contact.getEmail().isEmpty()) {
-            mailBtn.setBackground(Improve.getInstance().getResources().getDrawable(R.drawable.ic_contact_email_grey));
+        name.text = contact.name
+        if (contact.email == null || contact.email.isEmpty()) {
+            mailBtn.background = ContextCompat.getDrawable(
+                instance!!,
+                R.drawable.ic_contact_email_grey
+            )
         } else {
-            mailBtn.setBackground(Improve.getInstance().getResources().getDrawable(R.drawable.ic_contact_email_active));
+            mailBtn.background =
+                ContextCompat.getDrawable(instance!!, R.drawable.ic_contact_email_active)
         }
-        mailBtn.setOnClickListener(this);
-
-        if (contact.getPhone() == null || contact.getPhone().isEmpty()) {
-            callBtn.setBackground(Improve.getInstance().getResources().getDrawable(R.drawable.ic_contact_mobile_grey));
+        mailBtn.setOnClickListener(this)
+        if (contact.phone == null || contact.phone.isEmpty()) {
+            callBtn.background =
+                ContextCompat.getDrawable(instance!!, R.drawable.ic_contact_mobile_grey)
         } else {
-            callBtn.setBackground(Improve.getInstance().getResources().getDrawable(R.drawable.ic_contact_mobile_active));
+            callBtn.background =
+                ContextCompat.getDrawable(instance!!, R.drawable.ic_contact_mobile_active)
         }
-        callBtn.setOnClickListener(this);
+        callBtn.setOnClickListener(this)
         // [END] Define each view
+        mView.setOnClickListener {
+            val bundle = createBundle(contact)
+            val contactDetailsSheetFragment = ContactDetailsSheetFragment()
+            contactDetailsSheetFragment.arguments = bundle
+            contactDetailsSheetFragment.show(
+                instance!!.mainActivityRef!!.supportFragmentManager,
+                contactDetailsSheetFragment.tag
+            )
+        }
+    }
 
-        mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle bundle = createBundle(contact);
-                ContactDetailsSheetFragment contactDetailsSheetFragment = new ContactDetailsSheetFragment();
-                contactDetailsSheetFragment.setArguments(bundle);
-                contactDetailsSheetFragment.show(Improve.getInstance().getMainActivityRef().getSupportFragmentManager(),
-                        contactDetailsSheetFragment.getTag());
+    private fun createBundle(contact: Contact): Bundle {
+        val bundle = Bundle()
+        bundle.putParcelable(ContactDetailsSheetFragment.CONTACT_KEY, contact)
+        bundle.putInt(ContactDetailsSheetFragment.PARENT_FRAGMENT_KEY, R.integer.CONTACT_FRAGMENT)
+        return bundle
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.call_contact_btn -> if (contact!!.phone == null || contact!!.phone.isEmpty()) {
+                Toast.makeText(
+                    instance,
+                    instance!!.resources.getString(R.string.contact_no_phone_message),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                val callIntent = Intent(Intent.ACTION_DIAL)
+                callIntent.data = Uri.parse("tel:" + contact!!.phone)
+                callIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                instance!!.startActivity(callIntent)
             }
-        });
-    }
-
-    private Bundle createBundle(Contact contact) {
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(ContactDetailsSheetFragment.CONTACT_KEY, contact);
-        bundle.putInt(ContactDetailsSheetFragment.PARENT_FRAGMENT_KEY, R.integer.CONTACT_FRAGMENT);
-        return bundle;
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.call_contact_btn:
-                if(contact.getPhone() == null || contact.getPhone().isEmpty()) {
-                    Toast.makeText(Improve.getInstance(),
-                            Improve.getInstance().getResources().getString(R.string.contact_no_phone_message),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                    callIntent.setData(Uri.parse("tel:" + contact.getPhone()));
-                    callIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
-                    Improve.getInstance().startActivity(callIntent);
-                }
-                break;
-            case R.id.mail_contact_btn:
-                if(contact.getEmail() == null || contact.getEmail().isEmpty()) {
-                    Toast.makeText(Improve.getInstance(),
-                            Improve.getInstance().getResources().getString(R.string.contact_no_email_message),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent mailIntent = new Intent(Intent.ACTION_SENDTO);
-                    mailIntent.setData(Uri.parse("mailto:" + contact.getEmail()));
-                    mailIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
-                    Improve.getInstance().startActivity(mailIntent);
-                }
-                break;
-            default:
-                break;
+            R.id.mail_contact_btn -> if (contact!!.email == null || contact!!.email.isEmpty()) {
+                Toast.makeText(
+                    instance,
+                    instance!!.resources.getString(R.string.contact_no_email_message),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                val mailIntent = Intent(Intent.ACTION_SENDTO)
+                mailIntent.data = Uri.parse("mailto:" + contact!!.email)
+                mailIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                instance!!.startActivity(mailIntent)
+            }
+            else -> {}
         }
     }
 }
