9ae0b90d1bc33059e88ca7fb7925f33c7ece762c
Daniel Holmberg <danielholmberg.dev@gmail.com>
2023-01-13 15:26:42 +0100

    Converts DriveServiceHelper to Kotlin

--- app/src/main/java/dev/danielholmberg/improve/Services/DriveServiceHelper.kt
+++ app/src/main/java/dev/danielholmberg/improve/Services/DriveServiceHelper.kt
@@ -1,164 +1,153 @@
-package dev.danielholmberg.improve.Services;
+package dev.danielholmberg.improve.Services
 
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.OpenableColumns;
-
-import androidx.core.util.Pair;
-
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
-import com.google.api.client.http.ByteArrayContent;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import com.google.api.services.drive.Drive
+import com.google.android.gms.tasks.Tasks
+import com.google.api.client.http.ByteArrayContent
+import com.google.api.services.drive.model.FileList
+import android.content.Intent
+import android.content.ContentResolver
+import android.net.Uri
+import android.provider.OpenableColumns
+import androidx.core.util.Pair
+import com.google.android.gms.tasks.Task
+import com.google.api.services.drive.model.File
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 /**
  * A utility for performing read/write operations on Drive files via the REST API and opening a
  * file picker UI via Storage Access Framework.
  */
-public class DriveServiceHelper {
-    private final Executor mExecutor = Executors.newSingleThreadExecutor();
-    private final Drive mDriveService;
-
-    public static final String TYPE_NOTE = "text/plain";
-    public static final String TYPE_CONTACT = "text/vcard";
-
-    public DriveServiceHelper(Drive driveService) {
-        mDriveService = driveService;
-    }
+class DriveServiceHelper(private val mDriveService: Drive) {
+    private val mExecutor: Executor = Executors.newSingleThreadExecutor()
 
     /**
      * Creates a text file in the user's My Drive folder and returns its file ID.
      */
-    public Task<String> createFile(String mimeType, String fileName, String content) {
-        return Tasks.call(mExecutor, () -> {
-            File metadata = new File()
-                    .setParents(Collections.singletonList("root"))
-                    .setMimeType(mimeType)
-                    .setName(fileName);
+    fun createFile(mimeType: String?, fileName: String?, content: String?): Task<String> {
+        return Tasks.call(mExecutor) {
+            val metadata = File()
+                .setParents(listOf("root"))
+                .setMimeType(mimeType)
+                .setName(fileName)
 
             // Convert content to an AbstractInputStreamContent instance.
-            ByteArrayContent contentStream = ByteArrayContent.fromString(mimeType, content);
-
-            File googleFile = mDriveService.files().create(metadata, contentStream).execute();
-            if (googleFile == null) {
-                throw new IOException("Null result when requesting file creation.");
-            }
-
-            return googleFile.getId();
-        });
+            val contentStream = ByteArrayContent.fromString(mimeType, content)
+            val googleFile = mDriveService.files().create(metadata, contentStream).execute()
+                ?: throw IOException("Null result when requesting file creation.")
+            googleFile.id
+        }
     }
 
     /**
-     * Opens the file identified by {@code fileId} and returns a {@link Pair} of its name and
+     * Opens the file identified by `fileId` and returns a [Pair] of its name and
      * contents.
      */
-    public Task<Pair<String, String>> readFile(String fileId) {
-        return Tasks.call(mExecutor, () -> {
+    fun readFile(fileId: String?): Task<Pair<String, String>> {
+        return Tasks.call(mExecutor) {
+
             // Retrieve the metadata as a File object.
-            File metadata = mDriveService.files().get(fileId).execute();
-            String name = metadata.getName();
-
-            // Stream the file contents to a String.
-            try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
+            val metadata = mDriveService.files()[fileId].execute()
+            val name = metadata.name
+            mDriveService.files()[fileId].executeMediaAsInputStream().use { `is` ->
+                BufferedReader(
+                    InputStreamReader(`is`)
+                ).use { reader ->
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+                    while (reader.readLine().also { line = it } != null) {
+                        stringBuilder.append(line)
+                    }
+                    val contents = stringBuilder.toString()
+                    return@call Pair.create(name, contents)
                 }
-                String contents = stringBuilder.toString();
-
-                return Pair.create(name, contents);
             }
-        });
+        }
     }
 
     /**
-     * Updates the file identified by {@code fileId} with the given {@code name} and {@code
-     * content}.
+     * Updates the file identified by `fileId` with the given `name` and `content`.
      */
-    public Task<Void> saveFile(String fileId, String name, String content) {
-        return Tasks.call(mExecutor, () -> {
+    fun saveFile(fileId: String?, name: String?, content: String?): Task<Void?> {
+        return Tasks.call(mExecutor) {
+
             // Create a File containing any metadata changes.
-            File metadata = new File().setName(name);
+            val metadata = File().setName(name)
 
             // Convert content to an AbstractInputStreamContent instance.
-            ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
+            val contentStream = ByteArrayContent.fromString("text/plain", content)
 
             // Update the metadata and contents.
-            mDriveService.files().update(fileId, metadata, contentStream).execute();
-            return null;
-        });
+            mDriveService.files().update(fileId, metadata, contentStream).execute()
+            null
+        }
     }
 
     /**
-     * Returns a {@link FileList} containing all the visible files in the user's My Drive.
+     * Returns a [FileList] containing all the visible files in the user's My Drive.
      *
-     * <p>The returned list will only contain files visible to this app, i.e. those which were
+     *
+     * The returned list will only contain files visible to this app, i.e. those which were
      * created by this app. To perform operations on files not created by the app, the project must
-     * request Drive Full Scope in the <a href="https://play.google.com/apps/publish">Google
-     * Developer's Console</a> and be submitted to Google for verification.</p>
+     * request Drive Full Scope in the [Google
+ * Developer's Console](https://play.google.com/apps/publish) and be submitted to Google for verification.
      */
-    public Task<FileList> queryFiles() {
-        return Tasks.call(mExecutor, () ->
-                mDriveService.files().list().setSpaces("drive").execute());
+    fun queryFiles(): Task<FileList> {
+        return Tasks.call(mExecutor) { mDriveService.files().list().setSpaces("drive").execute() }
     }
 
     /**
-     * Returns an {@link Intent} for opening the Storage Access Framework file picker.
+     * Returns an [Intent] for opening the Storage Access Framework file picker.
      */
-    public Intent createFilePickerIntent(String mimeType) {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType(mimeType);
-
-        return intent;
+    fun createFilePickerIntent(mimeType: String?): Intent {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = mimeType
+        return intent
     }
 
     /**
-     * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
-     * created by {@link #createFilePickerIntent(String)} using the given {@code contentResolver}.
+     * Opens the file at the `uri` returned by a Storage Access Framework [Intent]
+     * created by [.createFilePickerIntent] using the given `contentResolver`.
      */
-    public Task<Pair<String, String>> openFileUsingStorageAccessFramework(
-            ContentResolver contentResolver, Uri uri) {
-        return Tasks.call(mExecutor, () -> {
+    fun openFileUsingStorageAccessFramework(
+        contentResolver: ContentResolver, uri: Uri?
+    ): Task<Pair<String?, String?>> {
+        return Tasks.call(mExecutor) {
+
             // Retrieve the document's display name from its metadata.
-            String name;
-            try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
-                if (cursor != null && cursor.moveToFirst()) {
-                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                    name = cursor.getString(nameIndex);
+            var name: String
+            contentResolver.query(uri!!, null, null, null, null).use { cursor ->
+                name = if (cursor != null && cursor.moveToFirst()) {
+                    val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    cursor.getString(nameIndex)
                 } else {
-                    throw new IOException("Empty cursor returned for file.");
+                    throw IOException("Empty cursor returned for file.")
                 }
             }
 
             // Read the document's contents as a String.
-            String content;
-            try (InputStream is = contentResolver.openInputStream(uri);
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
+            var content: String
+            contentResolver.openInputStream(uri).use { `is` ->
+                BufferedReader(InputStreamReader(`is`)).use { reader ->
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+                    while (reader.readLine().also { line = it } != null) {
+                        stringBuilder.append(line)
+                    }
+                    content = stringBuilder.toString()
                 }
-                content = stringBuilder.toString();
             }
+            Pair.create(name, content)
+        }
+    }
 
-            return Pair.create(name, content);
-        });
+    companion object {
+        const val TYPE_NOTE = "text/plain"
+        const val TYPE_CONTACT = "text/vcard"
     }
 }
